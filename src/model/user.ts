dd88d1e819cdaa8214e7b5fd4095b512c409def5
Uddin <maryam@192.168.101.5>
2026-01-03 10:08:16 +0545

    starting

---
+++ src/model/user.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export  interface Message extends Document {
+    content: string;
+    createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+    content: {
+
+        type: String,
+        required: true
+    },
+    
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: string;
+    isAcceptingMessage: boolean;
+    message: Message[];
+}
